e7f2f115a6cb576670f015c0a3764bbd6d8685c6
Shinecha3 <kittinan.sor@ku.th>
2025-07-14 21:49:30 +0700

    Add files via upload

--- lib/ShoppingCartCalculator.java
+++ lib/ShoppingCartCalculator.java
@@ -3,20 +3,23 @@ import java.util.ArrayList;
 public class ShoppingCartCalculator {
 
     /**
-     * เขียน Javadoc ที่นี่เพื่ออธิบายกฎการทำงานและกรณีพิเศษ:
-     * - จะทำอย่างไรถ้า items เป็น null หรือ empty?
-     * - จะทำอย่างไรถ้า CartItem มี price หรือ quantity ติดลบ?
+     * ราคารวมพื้นฐาน: คำนวณจาก price * quantity ของสินค้านั้น
      * - กฎส่วนลด BOGO (ซื้อ 1 แถม 1)
      * - กฎส่วนลด BULK (ซื้อ >= 6 ชิ้น ลด 10%)
-     * @param  ArrayList<CarItem> เข้ามาเป็นรายการสินค้า
+     * @param  ArrayList<CarItem> เข้ามาเป็นรายการสินค้าโดยห้าม เป็น null empty และ {จำนวนรายการเกิน 1หมื่นรายการ และรายการละไม่เกิน 10ล้านชิ้น ราคาสินค้าต่อชิ้นห้ามเกิน 100ล้าน}
      * @return ผลลัพจากการคำนวนราคาสินค้าแบบมีส่วนลดกับไม่มีส่วนลด
      */
     public static double calculateTotalPrice(ArrayList<CartItem> items) {
         // TODO: เขียนโค้ด Implementation ที่นี่
         double price = 0 ,total = 0 ,quantity = 0;
-        if (items != null && !items.isEmpty()) {
-            
+        if (items != null && !items.isEmpty() && items.size() < 10001) {
             for(int i=0; i<items.size();i++){
+                if (items.get(i).quantity() > 10000001 || items.get(i).price() > 100000001) {
+                    return 0.0;                                                  
+                }
+                if (items.get(i).quantity() < 1 || items.get(i).price() < 1) {
+                    return 0.0;                    
+                }
                 if (items.get(i).sku() == "BOGO") {
                     quantity = items.get(i).quantity();
                     if (quantity >1) {
