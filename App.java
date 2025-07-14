784ae4285ccc3c8787663ed9f67781caa1c41288
Shinecha3 <kittinan.sor@ku.th>
2025-07-14 21:49:46 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String [] args){
-        System.out.println("Program Start");
+        //System.out.println("Program Start");
         ShoppingCartManualTest.run();
     }
 }
